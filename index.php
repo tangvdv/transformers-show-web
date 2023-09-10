2f4b928d3e3e20b2ae753bb3a8a89eaec1f9129f
TangVdv <tangrider77120@gmail.com>
2023-09-10 12:44:49 +0200

    Setup basic index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include_once __DIR__ . "/src/includes/head.php" ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+<?php include_once __DIR__."/src/includes/header.php"; ?>
+</body>
+</html>
