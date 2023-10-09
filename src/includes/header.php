9a7a6abafb47c76f559042bab3c6bf131743dfd7
TangVdv <tangrider77120@gmail.com>
2023-10-09 16:35:12 +0200

    '/' redirect in header

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,4 +1,7 @@
 <header class="d-flex justify-content-center py-3 app-bg-color-red">
+    <a href="/" class="mb-3 nav-link text-light border rounded-1">
+        <span class="fs-4 mx-2"><b>TF Show</b></span>
+    </a>
     <ul class="nav nav-pills">
         <li class="nav-item"><a href="/src/show/index.php" class="nav-link text-light" aria-current="page">Show</a></li>
         <li class="nav-item"><a href="/src/bot/index.php" class="nav-link text-light">Bot</a></li>
