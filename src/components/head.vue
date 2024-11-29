4cdfee9281342738db734db0f6ad46e9efb4b361
TangVdv <tangrider77120@gmail.com>
2024-11-29 16:19:53 +0100

    Removed Bootstrap

--- src/components/head.vue
+++ src/components/head.vue
@@ -1,6 +1,5 @@
 <template>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="/src/includes/css/tf-show.css">
+    <link href="/src/assets/css/tfshow.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ title }} </title>
@@ -15,13 +14,6 @@
         },
 
         mounted() {
-            const script = document.createElement('script');
-            script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js';
-            script.integrity = 'sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm';
-            script.crossOrigin = 'anonymous';
-
-            document.head.appendChild(script);
-
             this.title = document.head.getAttribute('title')
         }
     }
