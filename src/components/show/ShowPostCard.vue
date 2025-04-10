1785f8447568c352acfa923dfbbd2827b8eab0f9
TangVdv <tangrider77120@gmail.com>
2025-04-10 17:59:03 +0200

    Added auto resize text based on a desired max height

--- src/components/show/ShowPostCard.vue
+++ src/components/show/ShowPostCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <router-link :class="[ !isVisible ? 'visually-hidden' : '', 'poster-div', 'card', 'shadow-sm', 'mx-4', 'p-0' ]" :to="'/show/'+id">
-      <img class="poster rounded-2" :src="path + image">
-      <div class="card-body">
-          <p class="card-text text-center fs-5"> {{ show_name }} </p>
+  <router-link :class="[ !isVisible ? 'visually-hidden' : '', 'nav' ]" :to="'/show/'+id">
+    <div ref="card" class="show-card shadow flex column justify-content-between">
+      <img ref="cardImage" :src="path + image">
+      <div class="flex-grow flex column justify-content-center">
+          <p ref="cardTitle" :style="`font-size: ${fontSize}px`" class="show-card-title">{{ show_name }} </p>
       </div>
-    </router-link>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -14,8 +16,29 @@ export default {
   data() {
     return {
       path: `${import.meta.env.VITE_WEBSITE_URL}/images/show/`,
-      isVisible: true
+      isVisible: true,
+      fontSize: 16
     };
+  },
+  methods: {
+
+    resize(computedStyle, desired_height){
+      if(parseFloat(computedStyle.height) > desired_height){
+        this.fontSize = this.fontSize - 1;
+        this.$nextTick(() => {
+          this.resize(
+            window.getComputedStyle(this.$refs.cardTitle), 
+            desired_height
+          )
+        })
+      }
+    }
+  },
+  mounted() {
+    var desired_height = this.$refs.card.clientHeight - this.$refs.cardImage.clientHeight;
+    var computedStyle = window.getComputedStyle(this.$refs.cardTitle);
+
+    this.resize(computedStyle, desired_height);
   }
 }
 </script>
